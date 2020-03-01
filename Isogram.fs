8c98d2e0e9ced41091fe37e74e1f3873d6846622
theCADO <carlbrief@googlemail.com>
2020-03-01 16:54:21 +0100

    done

---
+++ Isogram.fs
@@ -0,0 +1,8 @@
+﻿module Isogram
+
+let isIsogram str = 
+    let valid = [ 'A' .. 'Z' ]
+    let upper = Seq.map System.Char.ToUpper str
+    let upperChar = Seq.filter (fun x -> List.contains x valid) upper
+    let unique = Seq.distinct upperChar
+    (Seq.length upperChar = Seq.length unique)
---
+++ SumOfMultiples.fs
@@ -0,0 +1,10 @@
+﻿module SumOfMultiples
+
+let sum (numbers: int list) (upperBound: int): int = 
+    if (upperBound > 0) then
+        let allMultiplesList = List.map (fun x -> (if(x > 0) then seq{0 .. x .. (upperBound - 1)} else seq{0}) |> Seq.toList) numbers
+        let allMultiples = List.concat allMultiplesList |> Seq.ofList
+        let uniqueMultiples = Seq.distinct allMultiples
+        printf "%A" (uniqueMultiples |> Seq.toList)
+        Seq.sum uniqueMultiples
+    else 0
